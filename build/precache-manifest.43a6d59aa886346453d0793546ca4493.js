45b693b7c15f4b2786ae58bfdd32c18fc5f314bf
Biljana Hrvacanin <huaweibica@gmail.com>
2020-10-20 00:16:17 +0200

    add project

---
+++ build/precache-manifest.43a6d59aa886346453d0793546ca4493.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc08d4f2ce490ed755006ffd44f4df93",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "7c1d47a9fd2729ad5dca",
+    "url": "/keeper-app/static/js/2.9060a03d.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/keeper-app/static/js/2.9060a03d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c63bc83e4c645a2df55",
+    "url": "/keeper-app/static/js/main.30a2ec91.chunk.js"
+  },
+  {
+    "revision": "65684a1bb1c1608d5cdc",
+    "url": "/keeper-app/static/js/runtime-main.9b153e5e.js"
+  }
+]);
